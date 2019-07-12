d52fc6a94c7ab2d97c478da8230eca60ccfb5c83
Ermiyas Wondimu <vectonv@gmail.com>
2019-07-12 16:39:14 +0100

    navigation bar css improved

--- client/src/components/NavigationBar.js
+++ client/src/components/NavigationBar.js
@@ -5,19 +5,26 @@ import cyf_brand from "../picture/cyf_brand.png";
 export class NavigationBar extends Component {
   render() {
     return (
-      <div className="nav-bar">
-        <div className='page-login'><h4 >login</h4></div>
-        
-        {<img className="image" src={cyf_brand} alt="cyf-brand" />}
-        <ul className="nav-top">
-          <li className="nav-item1">
-            <Link to="#">Sign In </Link>
-          </li>
-          |
-          <li className="nav-item2">
-            <Link to="#">Sign Up</Link>
-          </li>
-        </ul>
+      <div className="logo-nav">
+        <input type="checkbox" id="nav-check" />
+        <div className="logo-nav-header">
+          <div className="logo-nav-title">
+            <img className="image" src={cyf_brand} alt="cyf-brand" />
+          </div>
+        </div>
+        <div className="logo-nav-btn">
+          <label for="nav-check">
+            <span />
+            <span />
+            <span />
+          </label>
+        </div>
+
+        <div className="logo-nav-links">
+          <Link to="#">Sign In </Link>|
+         
+          <Link to="#">Sign Up</Link>
+        </div>
       </div>
     );
   }
